e3916dc17cd55c00a4fc6c5ed93bfd7cb2fac3f5
alinso <Edison>
2017-11-23 21:19:00 +0300

    FirstImplementation

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,41 @@
-public class {
+public class InsertNodeInLinkedList {
+
+
+    Node head;
+    class Node{
+        int data;
+        Node next;
+
+        Node(int d){
+            data=d;
+            next=null;
+        }
+
+
+        public void push(int newData){
+            Node newNode  = new Node(newData);
+            newNode.next  = head;
+            head = newNode;
+        }
+
+        public  void append(int newData){
+
+            Node newNode = new Node(newData);
+
+            if(head==null){
+                head = newNode;
+                return;
+            }
+
+            newNode.next  =null;
+
+            Node last  =head;
+            while(last.next!=null)
+                last = last.next;
+
+            last.next = newNode;
+        }
+    }
 
 
 }
